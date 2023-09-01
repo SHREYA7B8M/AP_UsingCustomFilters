cdb4f2cdfb823f0edb26c934b04678c148c110d8
s66829020@gmail.com <s66829020@gmail.com>
2023-09-01 22:56:35 +0530

    first commit

---
+++ UsingCustomFilters/UsingCustomFilters/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+
+
+namespace UsingCustomFilters.Controllers
+{
+    [ActionLoggingFilter]
+    public class HomeController : Controller
+    {
+        public object CookieAuthenticationDefaults { get; private set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [AllowAnonymous]
+        public ViewResult StudentView()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+
+        }
+
+        [Authorize]
+        public ViewResult StudentViewSecure()
+        {
+            return View();
+
+        }
+
+
+       
+    }
+}
+
+    
